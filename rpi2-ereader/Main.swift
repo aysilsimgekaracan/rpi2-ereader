37915dbded4197b02838b539aa76007928dbd762
Ayşıl Simge Karacan <github@aysilsimge.dev>
2026-09-19 21:34:54 +0300

    init project with blinking led example

---
+++ rpi2-ereader/Main.swift
@@ -0,0 +1,14 @@
+@main
+struct Main {
+  static func main() {
+    let led = UInt32(PICO_DEFAULT_LED_PIN)
+    gpio_init(led)
+    gpio_set_dir(led, true)
+    while true {
+      gpio_put(led, true)
+      sleep_ms(250)
+      gpio_put(led, false)
+      sleep_ms(250)
+    }
+  }
+}
---
+++ rpi2-ereader/README.md
@@ -0,0 +1,5 @@
+```bash
+rm -rf build
+cmake -B build -G Ninja .
+cmake --build build
+```
